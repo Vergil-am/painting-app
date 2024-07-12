e5dc3f7bee0dca35d88240cf73898a6a8205d3e9
vergil-am <amine.madman2@gmail.com>
2024-07-12 17:05:18 +0100

    .

--- app/users/route.ts
+++ app/users/route.ts
@@ -1,18 +1,19 @@
+import { NextResponse } from "next/server"
 
 
 
 
 export function GET(request: Request) {
-    const data = {
-        response : "success"
-    }
-    return Response.json({data})
+  const data = {
+    response: "success"
+  }
+  return NextResponse.json({ data })
 }
 
 
 export function POST(request: Request) {
-const data = {
-        response : "Posted successfully"
-    }
-    return Response.json({data})
+  const data = {
+    response: "Posted successfully"
+  }
+  return NextResponse.json({ data })
 }
