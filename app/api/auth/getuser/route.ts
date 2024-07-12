d52b8cf789aee2cf17b388a2b11c4b9621d0d8bc
vergil-am <amine.madman2@gmail.com>
2024-07-12 17:10:38 +0100

    .

--- app/api/auth/getuser/route.ts
+++ app/api/auth/getuser/route.ts
@@ -1,17 +1,18 @@
 import { getUser } from "@/lib/actions/clients"
 import argon2 from "argon2"
+import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
-    const {email, password} =await req.json()
+  const { email, password } = await req.json()
 
-    const user = await getUser(email)
-    if (!user) {
-        throw new Error("Email or password wrong")
-    }
-    const verify = await argon2.verify(user.password, password)
-    if (verify) {
-    return Response.json(user)
-    } else {
-        throw new Error("Email or passowrd wrong")
-    }
+  const user = await getUser(email)
+  if (!user) {
+    throw new Error("Email or password wrong")
+  }
+  const verify = await argon2.verify(user.password, password)
+  if (verify) {
+    return NextResponse.json(user)
+  } else {
+    throw new Error("Email or passowrd wrong")
+  }
 }
