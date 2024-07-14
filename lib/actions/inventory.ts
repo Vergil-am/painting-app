f6205a3ce25d937c068edb4a59240d8102271219
vergil-am <amine.madman2@gmail.com>
2024-07-14 17:12:43 +0100

    .

--- lib/actions/inventory.ts
+++ lib/actions/inventory.ts
@@ -1,38 +1,38 @@
-import {db} from "@/db/index"
+import { db } from "@/db/index"
 import { InsertInventory, SelectInventory, inventory } from "@/db/schema"
 import { eq } from "drizzle-orm"
 
 export async function getInventory() {
-    try {
+  try {
     const items = await db.select({
-        inventory
+      inventory
     }).from(inventory).execute()
 
     return items
-} catch(e ) {
+  } catch (e) {
     return []
-}
+  }
 }
 
 
 export async function addInventory(item: InsertInventory) {
-    try {
-        const newItem = await db.insert(inventory).values(item).returning({insertedId: inventory.id}).execute()
-        console.log(newItem)
-        return newItem
-    } catch (e) {
-        return []
-    }
-    
+  try {
+    const newItem = await db.insert(inventory).values(item).returning({ insertedId: inventory.id }).execute()
+    console.log(newItem)
+    return newItem
+  } catch (e) {
+    return []
+  }
+
 }
 
 
-export async function getItem(id: number) {
-    try {
-        const item = await db.select().from(inventory).where(eq(inventory.id, id)).execute()
-        console.log(item)
-        return item
-    } catch (e) {
-        console.log(e)
-    }
-}
+// export async function getItem(id: number) {
+//     try {
+//         const item = await db.select().from(inventory).where(eq(inventory.id, id)).execute()
+//         console.log(item)
+//         return item
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
