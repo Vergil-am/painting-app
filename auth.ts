3686525cba769598dd02b117e477b569e17509d9
vergil-am <amine.madman2@gmail.com>
2024-07-14 17:05:29 +0100

    .

--- auth.ts
+++ auth.ts
@@ -1,37 +1,37 @@
 import NextAuth, { User } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
- 
-export const { handlers , signIn, signOut, auth } = NextAuth({
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
     signIn: "/auth/signin",
   },
   providers: [
     Credentials({
       credentials: {
-        email: {label: "email", name : "email" },
-        password : {label: "password", type: "password", name: "password"}
+        email: { label: "email", name: "email" },
+        password: { label: "password", type: "password", name: "password" }
       },
       authorize: async (credentials) => {
-          let user = null
-          const  res = await fetch('http://localhost:3000/api/auth/getuser', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(credentials),
-          });
-          user = await res.json()
-           
-          return user 
+        let user = null
+        const res = await fetch('http://localhost:3000/api/auth/getuser', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(credentials),
+        });
+        user = await res.json()
+
+        return user
       }
     }
-  )
+    )
   ],
   callbacks: {
-    jwt({token, user }) {
+    jwt({ token, user }) {
       return token
     },
 
-    session({session, token}) {
-      session.user.id = token.sub
+    session({ session, token }) {
+      session.user.id = token.sub as string
       return session
     }
   },
