080469b4b6378ec1fb9ee1ccceb345cf1f297a95
vergil-am <amine.madman2@gmail.com>
2024-09-06 16:04:23 +0100

    .

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -14,8 +14,6 @@ export default async function Orders() {
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <h1 className="text-3xl font-bold mb-6">Order History</h1>
-
-      //@ts-ignore
       <OrdersTable orders={orders != undefined ? orders : []} />
     </div>
   )
