877339f4206ac25c9bd64ff683fa2040e986355d
vergil-am <amine.madman2@gmail.com>
2024-09-27 16:41:02 +0100

    more admin tables

--- components/admin/AdminInventoryTable.tsx
+++ components/admin/AdminInventoryTable.tsx
@@ -1,30 +1,112 @@
 "use client"
 import { Table, TableHeader, TableColumn, TableBody, TableCell, TableRow } from "@nextui-org/table"
-import { SelectInventory } from "@/db/schema"
+import { SelectInventory, inventory } from "@/db/schema"
+import { useCallback } from "react";
+import { User } from "@nextui-org/user";
+import { Chip } from "@nextui-org/chip"
+import { Tooltip } from "@nextui-org/tooltip";
+import { ChipProps } from "@nextui-org/chip";
+import { EyeIcon, EditIcon, DeleteIcon } from "lucide-react";
 
+const columns = [
+  { name: "NAME", uid: "item_name" },
+  { name: "QUANTITY", uid: "quantity" },
+  { name: "ACTIONS", uid: "actions" },
+];
+
+type Item = SelectInventory;
 export default function InventoryTable({ inventory }: { inventory: SelectInventory[] }) {
-  return (
-    <Table aria-label="Example static collection table">
-      <TableHeader>
-        <TableColumn>ID</TableColumn>
-        <TableColumn>Name</TableColumn>
-        <TableColumn>Quantity</TableColumn>
-        <TableColumn>Threshold</TableColumn>
-      </TableHeader>
-      <TableBody emptyContent={"No items to display"}>
-        {inventory.map((item) => {
-          return (
-            <TableRow key={item.id}>
-              <TableCell>{item.id}</TableCell>
-              <TableCell>{item.item_name}</TableCell>
-              <TableCell>{item.quantity}</TableCell>
-              <TableCell>{item.threshold}</TableCell>
-            </TableRow>
-          )
-        })}
+  const renderCell = useCallback((item: SelectInventory, columnKey: React.Key) => {
+    const cellValue = item[columnKey as keyof Item];
 
+    switch (columnKey) {
+      case "item_name":
+        return (
+          <User
+            avatarProps={{ radius: "lg", src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjKWigz5N0C8Mb5l2QCBuo9pQRHjSPeq19qA&s" }}
+            // description={item.item_name}
+            name={cellValue}
+          >
+            {/* {item.item_name} */}
+          </User>
+        );
+      case "quantity":
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-sm capitalize">{cellValue}</p>
+            {/* <p className="text-bold text-sm capitalize text-default-400">{item.quantity}</p> */}
+          </div>
+        );
+      // case "status":
+      //   return (
+      //     <Chip className="capitalize" color={statusColorMap["vacation"]} size="sm" variant="flat">
+      //       {cellValue}
+      //     </Chip>
+      //   );
+      case "actions":
+        return (
+          <div className="relative flex items-center gap-2">
+            <Tooltip content="Details">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EyeIcon />
+              </span>
+            </Tooltip>
+            <Tooltip content="Edit user">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EditIcon />
+              </span>
+            </Tooltip>
+            <Tooltip color="danger" content="Delete user">
+              <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                <DeleteIcon />
+              </span>
+            </Tooltip>
+          </div>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
+  return (
+    <Table aria-label="Example table with custom cells">
+      <TableHeader columns={columns}>
+        {(column) => (
+          <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
+            {column.name}
+          </TableColumn>
+        )}
+      </TableHeader>
+      <TableBody items={inventory}>
+        {(item) => (
+          <TableRow key={item.id}>
+            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+          </TableRow>
+        )}
       </TableBody>
     </Table>
+
   )
 }
 
+
+{/* <Table aria-label="Example static collection table"> */ }
+{/*   <TableHeader> */ }
+{/*     <TableColumn>ID</TableColumn> */ }
+{/*     <TableColumn>Name</TableColumn> */ }
+{/*     <TableColumn>Quantity</TableColumn> */ }
+{/*     <TableColumn>Threshold</TableColumn> */ }
+{/*   </TableHeader> */ }
+{/*   <TableBody emptyContent={"No items to display"}> */ }
+{/*     {inventory.map((item) => { */ }
+{/*       return ( */ }
+{/*         <TableRow key={item.id}> */ }
+{/*           <TableCell>{item.id}</TableCell> */ }
+{/*           <TableCell>{item.item_name}</TableCell> */ }
+{/*           <TableCell>{item.quantity}</TableCell> */ }
+{/*           <TableCell>{item.threshold}</TableCell> */ }
+{/*         </TableRow> */ }
+{/*       ) */ }
+{/*     })} */ }
+{/**/ }
+{/*   </TableBody> */ }
+{/* </Table> */ }
--- components/admin/AdminOrdersTable.tsx
+++ components/admin/AdminOrdersTable.tsx
@@ -1,33 +1,138 @@
 "use client"
 import { Table, TableHeader, TableColumn, TableBody, TableCell, TableRow } from "@nextui-org/table"
 import { SelectOrder } from "@/db/schema"
+import { Chip } from "@nextui-org/chip"
+import { Tooltip } from "@nextui-org/tooltip";
+import { ChipProps } from "@nextui-org/chip";
+import { useCallback } from "react";
+import { User } from "@nextui-org/user";
+import { EyeIcon, EditIcon, DeleteIcon } from "lucide-react";
 
+
+const columns = [
+  { name: "NAME", uid: "name" },
+  { name: "ORDER ID", uid: "id" },
+  { name: "STATUS", uid: "status" },
+  { name: "CREATED AT", uid: "created_at" },
+  { name: "ACTIONS", uid: "actions" },
+];
+
+const statusColorMap: Record<string, ChipProps["color"]> = {
+  paid: "success",
+  error: "danger",
+  unpaid: "warning",
+};
+
+type Order = SelectOrder;
 export default function AdminOrdersTable({ orders }: { orders: SelectOrder[] }) {
+  const renderCell = useCallback((order: SelectOrder, columnKey: React.Key) => {
+    const cellValue = order[columnKey as keyof Order];
+
+    switch (columnKey) {
+      case "name":
+        return (
+          <User
+            avatarProps={{ radius: "lg", src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjKWigz5N0C8Mb5l2QCBuo9pQRHjSPeq19qA&s" }}
+            description={order.phone}
+
+            //@ts-ignore
+            name={cellValue}
+          >
+            {order.name}
+          </User>
+        );
+      case "quantity":
+        return (
+          <div className="flex flex-col">
+
+            {/* @ts-ignore */}
+            <p className="text-bold text-sm capitalize">{cellValue}</p>
+            {/* <p className="text-bold text-sm capitalize text-default-400">{item.quantity}</p> */}
+          </div>
+        );
+      case "created_at":
+        return (
+          <p>{order.created_at?.toLocaleDateString()}</p>
+        );
+      case "status":
+        return (
+
+          // @ts-ignore 
+          < Chip className="capitalize" color={statusColorMap[order.status]} size="sm" variant="flat" >
+            {/* @ts-ignore */}
+            {cellValue}
+          </Chip >
+        );
+      case "actions":
+        return (
+          <div className="relative flex items-center gap-2">
+            <Tooltip content="Details">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EyeIcon />
+              </span>
+            </Tooltip>
+            <Tooltip content="Edit user">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EditIcon />
+              </span>
+            </Tooltip>
+            <Tooltip color="danger" content="Delete user">
+              <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                <DeleteIcon />
+              </span>
+            </Tooltip>
+          </div>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
   return (
-    <Table aria-label="Example static collection table">
-      <TableHeader>
-        <TableColumn>Order Number</TableColumn>
-        <TableColumn>Quantity</TableColumn>
-        <TableColumn>Price</TableColumn>
-        <TableColumn>Oder date</TableColumn>
-        <TableColumn>STATUS</TableColumn>
+    <Table aria-label="Example table with custom cells">
+      <TableHeader columns={columns}>
+        {(column) => (
+          <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
+            {column.name}
+          </TableColumn>
+        )}
       </TableHeader>
-      <TableBody emptyContent={"No orders to display"}>
-        {orders.map((order) => {
-          return (
-            <TableRow key={order.id}>
-              <TableCell>{order.id}</TableCell>
-
-              <TableCell>1</TableCell>
-              <TableCell>29.99$</TableCell>
-              <TableCell>{order.created_at?.toDateString()}</TableCell>
-              <TableCell>{order.status}</TableCell>
-            </TableRow>
-          )
-        })}
-
+      <TableBody items={orders}>
+        {(item) => (
+          <TableRow key={item.id}>
+            {/* @ts-ignore */}
+            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+          </TableRow>
+        )}
       </TableBody>
     </Table>
+
   )
 }
+
+
+
+{/* <Table aria-label="Example static collection table"> */ }
+{/*   <TableHeader> */ }
+{/*     <TableColumn>Order Number</TableColumn> */ }
+{/*     <TableColumn>Quantity</TableColumn> */ }
+{/*     <TableColumn>Price</TableColumn> */ }
+{/*     <TableColumn>Oder date</TableColumn> */ }
+{/*     <TableColumn>STATUS</TableColumn> */ }
+{/*   </TableHeader> */ }
+{/*   <TableBody emptyContent={"No orders to display"}> */ }
+{/*     {orders.map((order) => { */ }
+{/*       return ( */ }
+{/*         <TableRow key={order.id}> */ }
+{/*           <TableCell>{order.id}</TableCell> */ }
+{/**/ }
+{/*           <TableCell>1</TableCell> */ }
+{/*           <TableCell>29.99$</TableCell> */ }
+{/*           <TableCell>{order.created_at?.toDateString()}</TableCell> */ }
+{/*           <TableCell>{order.status}</TableCell> */ }
+{/*         </TableRow> */ }
+{/*       ) */ }
+{/*     })} */ }
+{/**/ }
+{/*   </TableBody> */ }
+{/* </Table> */ }
 
